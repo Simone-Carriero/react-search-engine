9549851681a3c83b3e71d0181ce0ba48eeaf3ec8
Simone Carriero <simonecarriero95@hotmail.com>
2022-09-13 13:38:50 +0200

    Add settings to host the site on github

--- src/index.js
+++ src/index.js
@@ -4,17 +4,17 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { SearchProvider } from './contexts/SearchContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <SearchProvider>
         <App />
       </SearchProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
